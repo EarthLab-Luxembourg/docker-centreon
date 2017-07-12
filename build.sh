3661658b78ab72ab54b900397c1ac5177e5e0c18
Adam Cecile <adam.cecile@hitec.lu>
2017-07-12 13:09:37 +0200

    Ability to override VMWare PERL DSK filename

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ source CONFIG
 
 sed -e "s/@@HOSTNAME@@/${hostname}/g" \
     -e "s/@@SMTP_SERVER@@/${smtp_server}/g" \
+    -e "s/@@VMWARE_PERL_SDK@@/${vmware_perl_sdk}/g" \
     buildenv/Dockerfile.in > buildenv/Dockerfile
 
 docker build $@ -t ${container_name} -f buildenv/Dockerfile buildenv/
