e25f48d231361dd4c8c052a6c1b8679de21105a9
Adam Cecile <acecile@le-vert.net>
2017-10-10 12:27:17 +0200

    Ability to add custom rules in Dockerfile.local files.local

--- build.sh
+++ build.sh
@@ -7,4 +7,10 @@ sed -e "s/@@HOSTNAME@@/${hostname}/g" \
     -e "s/@@VMWARE_PERL_SDK@@/${vmware_perl_sdk}/g" \
     buildenv/Dockerfile.in > buildenv/Dockerfile
 
+if [ -f "buildenv/Dockerfile.local" ]; then
+  sed -i -e '/^@@DOCKERFILELOCAL@@$/{r buildenv/Dockerfile.local' -e 'd}' buildenv/Dockerfile
+else
+  sed -i 's/^@@DOCKERFILELOCAL@@$//' buildenv/Dockerfile
+fi
+
 docker build $@ -t ${container_name} -f buildenv/Dockerfile buildenv/
